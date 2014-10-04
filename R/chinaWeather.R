54e36d40dbe096b482454bebf3868fd7152c8e6a
bsspirit <bsspirit@163.com>
2014-10-05 01:59:31 +0800

    app

--- R/chinaWeather.R
+++ R/chinaWeather.R
@@ -5,8 +5,47 @@
 #' @keywords filename
 #' @export
 #' @examples
-#' filename()
-#' filename(as.Date("20110701",format="%Y%m%d"))
+#' \dontrun{
+#'  filename()
+#'  filename(as.Date("20110701",format="%Y%m%d"))
+#' }
 filename<-function(date=Sys.time()){
   paste(format(date, "%Y%m%d"),".csv",sep="")
+}
+
+#' Get WOEID of Yahoo By City Name
+#'
+#' @param en input a English city name
+#' @param zh input a Chinese city name
+#' @return integer WOEID
+#' @keywords WOEID
+#' @export
+#' @examples
+#' \dontrun{
+#'  getWOEIDByCity()
+#'  getWOEIDByCity(en="beijing")
+#'  getWOEIDByCity(zh='上海')
+#' }
+getWOEIDByCity<-function(en="beijing",zh=NULL){
+  if(!is.null(zh) && !is.na(zh) ){
+    return(WOEID$woeid[which(WOEID$zh==zh)])
+  }
+  return(WOEID$woeid[which(WOEID$en==en)])
+}
+
+#' Get all of city info
+#'
+#' @param lang input a language
+#' @return data.frame city info
+#' @keywords language
+#' @export
+#' @examples
+#' \dontrun{
+#'  getCityInfo()
+#'  getCityInfo(lang="en")
+#'  getCityInfo(lang="zh")
+#' }
+getCityInfo<-function(lang="en"){
+  if(lang=="en")return(WOEID[-c(3,4)])
+  if(lang=="zh")return(WOEID[-c(4)])
 }
